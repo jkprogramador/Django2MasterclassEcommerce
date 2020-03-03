6c894a4d8d791a78654211b27ef337d59a7d8545
Jack Lee <jack.lee.1981@pm.me>
2020-03-03 16:42:19 -0300

    Complete admin customization lesson.

--- shop/admin.py
+++ shop/admin.py
@@ -2,6 +2,22 @@ from django.contrib import admin
 
 from .models import Order, Product
 
+admin.site.site_header = 'E-commerce site'
+admin.site.site_title = 'ABC Shopping'
+admin.site.index_title = 'Manage ABC Shopping'
+
+class ProductAdmin(admin.ModelAdmin):
+
+  def change_category_to_default(self, request, queryset):
+    queryset.update(category = 'default')
+  
+  list_display = ('title', 'price', 'discount_price', 'category', 'description', 'image')
+  search_fields = ['title', 'category']
+  actions = ['change_category_to_default']
+  change_category_to_default.short_description = 'Default category'
+  fields = ['title', 'price']
+  list_editable = ['image', 'category']
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
